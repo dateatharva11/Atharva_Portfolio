01bb2ac1f1a621c658cbc45d7e7d54172e78c405
adate1 <adate1@asu.edu>
2025-06-19 22:31:30 -0700

    Minor bug fixes - Theme revamp and Buttons

--- atharva_react_portfolio/src/components/Hero/Hero.jsx
+++ atharva_react_portfolio/src/components/Hero/Hero.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 import { getImageUrl } from '../../utils';
 import styles from './Hero.module.css';
+import { Typewriter } from 'react-simple-typewriter';
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I am Atharva Date</h1>
+        <h1 className={styles.title}>
+          Hi, I am{' '}
+          <span style={{ color: '#64ffda' }}>
+            <Typewriter
+              words={['Atharva Date']}
+              loop={1}
+              cursor
+              cursorStyle="|"
+              typeSpeed={100}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+        </h1>
         <p className={styles.description}>I am a full stack developer with 4 years of experience 
           using frameworks such as React, Vue, Laravel and NodeJS. <br></br><br></br>
           Reach out if you'd like to learn more!
